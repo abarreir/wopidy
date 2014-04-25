5b7cf5a3dfd0ae282cceed2b3009ced6c31e7250
Alexandre Barreira <abarreir@gmail.com>
2014-04-25 20:13:19 +0200

    Listen to playback events

--- js/playback-controller.js
+++ js/playback-controller.js
@@ -18,6 +18,67 @@ var PlaybackController = function () {
     this.getTracklist = function() {
         return mopidy.tracklist.getTracks();
     };
+
+    mopidy.on("event:trackPlaybackPaused", function(data) {
+        console.log("Playback paused");
+		console.log(data);
+    });
+
+    mopidy.on("event:trackPlaybackResumed", function(data) {
+        console.log("Playback resumed");
+		console.log(data);
+    });
+
+    mopidy.on("event:trackPlaybackStarted", function(data) {
+        console.log("Playback started");
+		console.log(data);
+    });
+
+    mopidy.on("event:trackPlaybackEnded", function(data) {
+        console.log("Playback ended");
+		console.log(data);
+    });
+
+    mopidy.on("event:playbackStateChanged", function(data) {
+        console.log("Playback state changed");
+		console.log(data);
+    });
+
+    mopidy.on("event:tracklistChanged", function(data) {
+        console.log("Tracklist changed");
+		console.log(data);
+    });
+
+    mopidy.on("event:playlistsLoaded", function(data) {
+        console.log("Playlist loaded");
+		console.log(data);
+    });
+
+    mopidy.on("event:playlistChanged", function(data) {
+        console.log("Playlist changed");
+		console.log(data);
+    });
+
+    mopidy.on("event:optionsChanged", function(data) {
+        console.log("Options changed");
+		console.log(data);
+    });
+
+    mopidy.on("event:volumeChanged", function(data) {
+        console.log("Volume changed");
+		console.log(data);
+    });
+
+    mopidy.on("event:muteChanged", function(data) {
+        console.log("Mute changed");
+		console.log(data);
+    });
+
+    mopidy.on("event:seeked", function(data) {
+        console.log("Seeked");
+		console.log(data);
+    });
+
 };
 
 module.exports = new PlaybackController();
